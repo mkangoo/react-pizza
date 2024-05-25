178ea05273d4e85aaa7dceed98c662bcabb0863d
mkangoo <alexanderpavlov1408@gmail.com>
2024-05-26 02:10:51 +0300

    feat: added state for types and sizes

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
 
-function PizzaBlock({ title, price }) {
-    const [countPizza, setCountPizza] = useState(0)
+const typeNames = ['тонкое', 'традиционное']
+
+function PizzaBlock({ title, price, sizes, imageUrl }) {
+    const [pizzaCount, setPizzaCount] = useState(0)
+    const [activeSize, setActiveSize] = useState(0)
+    const [activeType, setActiveType] = useState(0)
 
     const isAddedPizza = () => {
-        setCountPizza(countPizza + 1)
+        setPizzaCount(pizzaCount + 1)
     }
     return (
-        <div class="pizza-block">
-            <img
-                class="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-                alt="Pizza"
-            />
-            <h4 class="pizza-block__title">{title}</h4>
-            <div class="pizza-block__selector">
+        <div className="pizza-block">
+            <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+            <h4 className="pizza-block__title">{title}</h4>
+            <div className="pizza-block__selector">
                 <ul>
-                    <li class="active">тонкое</li>
-                    <li>традиционное</li>
+                    {typeNames.map((type, index) => (
+                        <li onClick={() => setActiveType(index)} className={activeType === index ? 'active' : ''}>
+                            {type}
+                        </li>
+                    ))}
                 </ul>
                 <ul>
-                    <li class="active">26 см.</li>
-                    <li>30 см.</li>
-                    <li>40 см.</li>
+                    {sizes.map((size, index) => (
+                        <li key={size} onClick={() => setActiveSize(index)} className={activeSize === index ? 'active' : null}>
+                            {size} см.
+                        </li>
+                    ))}
                 </ul>
             </div>
-            <div class="pizza-block__bottom">
-                <div class="pizza-block__price">от {price} ₽</div>
-                <div class="button button--outline button--add" onClick={isAddedPizza}>
+            <div className="pizza-block__bottom">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add" onClick={isAddedPizza}>
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
@@ -35,8 +40,8 @@ function PizzaBlock({ title, price }) {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>{countPizza}</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     )
