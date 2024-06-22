8c7406567cd8daccd0095bc033cf38c61d2720e8
mkangoo <alexanderpavlov1408@gmail.com>
2024-06-22 19:19:49 +0300

    Init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import Header from '../components/Header'
+import Categories from '../components/Categories'
+import PizzaBlock from '../components/PizzaBlock'
+import { Skeleton } from '../components/PizzaBlock/Skeleton'
+import Sort from '../components/Sort'
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const API_BASE = process.env.REACT_APP_API_BASE_URL
+
+function Home() {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(`${API_BASE}/items`)
+                setItems(response.data)
+                setIsLoading(false)
+            } catch (err) {
+                alert('Ошибка при загрузке данных')
+                console.error(err)
+            }
+        }
+        fetchData()
+    }, [])
+    return (
+        <>
+            <div className="content__top">
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading
+                    ? [...Array(6)].map((_, index) => <Skeleton key={index} />)
+                    : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
+            </div>
+        </>
+    )
+}
+
+export default Home
