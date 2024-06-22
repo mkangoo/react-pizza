eec0a59d2f795760322055d2952209ef2d24fce7
mkangoo <alexanderpavlov1408@gmail.com>
2024-06-23 01:57:01 +0300

    feat: added not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,1 +1,8 @@
-import Header from '../components/Header'
+import NotFoundBlock from '../components/NotFoundBlock'
+
+
+function NotFound() {
+    return <NotFoundBlock />
+}
+
+export default NotFound
